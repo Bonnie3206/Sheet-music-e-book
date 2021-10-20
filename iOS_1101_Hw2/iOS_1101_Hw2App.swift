c45bc2ce8c48b4e0a30b86bdf5a6031742bc2f22
Bonnie3206 <molk3206@gmail.com>
2021-10-20 13:42:48 +0800

    Initial Commit

---
+++ iOS_1101_Hw2/iOS_1101_Hw2App.swift
@@ -0,0 +1,17 @@
+//
+//  iOS_1101_Hw2App.swift
+//  iOS_1101_Hw2
+//
+//  Created by CK on 2021/10/20.
+//
+
+import SwiftUI
+
+@main
+struct iOS_1101_Hw2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
